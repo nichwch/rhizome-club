b0a6af92ba713d30521dceea9a2b997586abd513
nichwch <nichwch@gmail.com>
2021-09-23 14:55:36 -0700

    change routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,22 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
 	return (
 		<div style={{ marginLeft: "auto", marginRight: "auto", width: "600px" }}>
-			<h1>RHIZOME</h1>
-			<p>"Since each of us was several, there was already quite a crowd."</p>
-			<h2>Rhizome is the home of the Davis Hypertext Society</h2>
+			<h1>RHIZOME.CLUB</h1>
+			<i>"Since each of us was several, there was already quite a crowd."</i>
+			<h2>rhizome.club is the online home of the Davis Hypertext Society</h2>
 			<p>The Davis Hypertext Society does 2 things.</p>
-			<h3>1. Website review</h3>
-			<p>Every week we meet to </p>
+			<h3>1. Website review (THEORY)</h3>
+			<p>
+				Every week, members pick a cool website (a site with extraordinarily
+				cool design/animations/content/vibes)by voting. We then meet up as a
+				group to discuss/critique the website, and look under the hood to see
+				how it's made.
+			</p>
+			<h3>2. Maintain rhizome.club (PRACTICE)</h3>
+			<p>
+				The current site is a loose amalgation of individual web pages submitted
+				by members. The site is open source, and
+			</p>
 		</div>
 	);
 }
