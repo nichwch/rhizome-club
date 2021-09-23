f1e9fbb51b04b3b0f8fd092319bbb1d3630d7cf5
nichwch <nichwch@gmail.com>
2021-09-23 14:40:01 -0700

    Create nick.tsx

---
+++ pages/people/nick.tsx
@@ -0,0 +1,20 @@
+const Nick = () => {
+	return (
+		<div>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+			<h1> Nicholas Chen</h1>
+		</div>
+	);
+};
+export default Nick;
