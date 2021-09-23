646cef7a694791d6dc5276597bc326a9e41da585
nichwch <nichwch@gmail.com>
2021-09-23 15:34:38 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ module.exports = {
 	// your project has type errors.
 	// !! WARN !!
 	ignoreBuildErrors: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 };
