d51f6d9f533a0d2b5f6ab8d9d1d875cc535a99ae
nichwch <nichwch@gmail.com>
2021-09-23 14:31:56 -0700

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<div>
+			<h1>RHIZOME</h1>
+			<p>"Since each of us was several, there was already quite a crowd."</p>
+		</div>
+	);
+}
